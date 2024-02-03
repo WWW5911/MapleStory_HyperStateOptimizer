fba2d1076dcfed66251dc94de8a5d7aeb152f5fa
Astria <hhh5911@gmail.com>
2024-02-03 20:20:55 +0800

    Add files via upload

--- js/DA.js
+++ js/DA.js
@@ -1,47 +1,13 @@
-function pointNeed(now){
-    switch (now){
-        case 1:
-            return 1
-        case 2:
-            return 2
-        case 3:
-            return 4
-        case 4:
-            return 8
-        case 5:
-            return 10
-        case 6:
-            return 15
-        case 7:
-            return 20
-        case 8:
-            return 25
-        case 9:
-            return 30
-        case 10:
-            return 35
-        case 11:
-            return 50
-        case 12:
-            return 65
-        case 13:
-            return 80
-        case 14:
-            return 95
-        case 15:
-            return 110
-        default:
-            return 1000000
-    }
-}
+class CharStatusDA {
+    constructor(HP, STR, HP_wo, HP_p, level, weapon, att, att_p, boss_a, defense, base_ig,  ctri_a,
+                p_HP = 0, p_STR = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0, ignore = -1) {
 
+        this.HP = HP;
+        this.STR = STR;
+        this.HP_wo = HP_wo;
+        this.level = level;
+        this.HP_p = HP_p;
 
-class CharStatus {
-    constructor(mainV, subV, weapon, att, att_p, boss_a, defense, base_ig,  ctri_a,
-                p_main = 0, p_sub = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0, ignore = -1) {
-
-        this.mainV = mainV;
-        this.subV = subV;
         this.weapon = weapon;
         this.att = att;
         this.att_p = att_p;
@@ -54,18 +20,24 @@ class CharStatus {
             this.ignore = ignore;
         this.ctri_a = ctri_a;
 
-        this.p_main = p_main
-        this.p_sub = p_sub
+        this.p_HP = p_HP
+        this.p_STR = p_STR
         this.p_ctri = p_ctri
         this.p_ignore = p_ignore
         this.p_harm = p_harm
         this.p_Bharm = p_Bharm
         this.p_atk = p_atk
 
+        this.HP_base = (this.HP - this.HP_wo) / (1+this.HP_p);
+
+    }
+    update_HP(){
+        this.HP = this.HP_base * (1+this.HP_p) + this.HP_wo;
     }
 
     base() {
-        return (4 * this.mainV + this.subV) * this.weapon * (this.att * (1 + this.att_p) ) * 0.01
+        this.update_HP();
+        return ( Math.floor( (this.level * 90 + 545) / 3.5 ) + 0.8 * Math.floor( (this.HP - (this.level * 90 + 545) )/3.5 ) ) * this.weapon * (this.att * (1 + this.att_p) ) * 0.01
     }
     defense_c() {
         var ret = 1 - this.defense * ( 1 - this.ignore) // (1-0.86) * (1-ignore)
@@ -81,62 +53,46 @@ class CharStatus {
     }
 
     clone() {
-        return new CharStatus(this.mainV, this.subV, this.weapon, this.att, this.att_p, this.boss_a, this.defense, this.base_ig, this.ctri_a,
-            this.p_main, this.p_sub, this.p_ctri, this.p_ignore, this.p_harm, this.p_Bharm, this.p_atk, this.ignore)
+        return new CharStatusDA(this.HP, this.STR, this.HP_wo, this.HP_p, this.level, this.weapon, this.att, this.att_p, this.boss_a, this.defense, this.base_ig, this.ctri_a,
+            this.p_HP, this.p_STR, this.p_ctri, this.p_ignore, this.p_harm, this.p_Bharm, this.p_atk, this.ignore)
     }
     get_points(){
-        return "" + this.p_main + this.p_sub + this.p_ctri + this.p_ignore + this.p_harm + this.p_Bharm + this.p_atk
+        return "" + this.p_HP + this.p_STR + this.p_ctri + this.p_ignore + this.p_harm + this.p_Bharm + this.p_atk
     }
 }
 
-
-// let dict = {} // 現在配點 : 攻擊
-// function dfs(charStatus, points){
-//     point_name = charStatus.get_points()
-//     if(  dict.hasOwnProperty( point_name)) 
-//         return;
-
-//     var new_atk = charStatus.realAtk();
-//     dict[point_name] = new_atk
-
-let max_v = 0
-let max_left = 0
-let max_status = null
-let set = new Set()
-let t = 1
-function dfs(charStatus, points){
+function dfsDA(charStatus, points){
     if(  set.has(charStatus.get_points() )) 
         return;
-    
     var new_atk = charStatus.realAtk();
     if( new_atk > max_v ){
         max_v = new_atk;
         max_left = points
         max_status = charStatus.clone();
-       // console.log(">>", charStatus.p_main, charStatus.p_sub, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
+       // console.log(">>", charStatus.p_HP, charStatus.p_STR, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
     }
-   // console.log(charStatus.p_main, charStatus.p_sub, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
+   // console.log(charStatus.p_HP, charStatus.p_STR, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
 
     document.getElementById("output").innerHTML = "計算中>< <br> 目前推演至：" ;
-    document.getElementById("output").innerHTML += "" + charStatus.p_main + " " + charStatus.p_sub + " "  +charStatus.p_ctri + " "  + charStatus.p_ignore  + " "  + charStatus.p_harm + " "  + charStatus.p_Bharm + " "  + charStatus.p_atk
+    document.getElementById("output").innerHTML += "" + charStatus.p_HP + " " + charStatus.p_STR + " "  +charStatus.p_ctri + " "  + charStatus.p_ignore  + " "  + charStatus.p_harm + " "  + charStatus.p_Bharm + " "  + charStatus.p_atk
 
     set.add( charStatus.get_points() )
 
-    // 主屬
-    point_next = pointNeed(charStatus.p_main+1)
+    // HP
+    point_next = pointNeed(charStatus.p_HP+1)
     if(points >= point_next ){
         var tmp = charStatus.clone();
-        tmp.p_main += 1
-        tmp.mainV += 30
-        dfs(tmp, points -  point_next )
+        tmp.p_HP += 1
+        tmp.HP_p += 0.02
+        dfsDA(tmp, points -  point_next )
     }
-    // 副屬
-    point_next = pointNeed(charStatus.p_sub+1)
+    // STR
+    point_next = pointNeed(charStatus.p_STR+1)
     if(points >= point_next ){
         var tmp = charStatus.clone();
-        tmp.p_sub += 1
-        tmp.subV += 30
-        dfs(tmp, points -  point_next )
+        tmp.p_STR += 1
+        tmp.STR += 30
+        dfsDA(tmp, points -  point_next )
     }
     // 爆傷
     point_next = pointNeed(charStatus.p_ctri+1)
@@ -144,7 +100,7 @@ function dfs(charStatus, points){
         var tmp = charStatus.clone();
         tmp.p_ctri += 1
         tmp.ctri_a += 0.01
-        dfs(tmp, points -  point_next )
+        dfsDA(tmp, points -  point_next )
     }
     // 無視
     point_next = pointNeed(charStatus.p_ignore+1)
@@ -153,7 +109,7 @@ function dfs(charStatus, points){
         tmp.ignore = tmp.base_ig
         tmp.p_ignore += 1
         tmp.add_ignore( tmp.p_ignore * 0.03 )
-        dfs(tmp, points -  point_next )
+        dfsDA(tmp, points -  point_next )
 
     }
     // 傷害
@@ -162,7 +118,7 @@ function dfs(charStatus, points){
         var tmp = charStatus.clone();
         tmp.p_harm += 1
         tmp.boss_a += 0.03
-        dfs(tmp, points -  point_next )
+        dfsDA(tmp, points -  point_next )
     }
     // B傷
     point_next = pointNeed(charStatus.p_Bharm+1)
@@ -173,7 +129,7 @@ function dfs(charStatus, points){
             tmp.boss_a += 0.03
         else
             tmp.boss_a += 0.04
-        dfs(tmp, points - point_next )
+        dfsDA(tmp, points - point_next )
 
     }
     // 攻擊
@@ -182,11 +138,11 @@ function dfs(charStatus, points){
         var tmp = charStatus.clone();
         tmp.p_atk += 1
         tmp.att += 3
-        dfs(tmp, points - point_next )
+        dfsDA(tmp, points - point_next )
     }
     
 }
-function optim_Greddy(charStatus, points){
+function optim_GreddyDA(charStatus, points){
     last_point = -1
 
     while(last_point != points && points > 0){
@@ -198,11 +154,11 @@ function optim_Greddy(charStatus, points){
 
 
         // 主屬
-        point_next = pointNeed(charStatus.p_main+1)
+        point_next = pointNeed(charStatus.p_HP+1)
         if(points >= point_next ){
             var tmp = charStatus.clone();
-            tmp.p_main += 1
-            tmp.mainV += 30
+            tmp.p_HP += 1
+            tmp.HP += 30
             tmp_rAtk = (tmp.realAtk() - based_atk) / point_next;
             if(tmp_rAtk > max_up){
                 max_up = tmp_rAtk;
@@ -210,11 +166,11 @@ function optim_Greddy(charStatus, points){
             }
         }
         // 副屬
-        point_next = pointNeed(charStatus.p_sub+1)
+        point_next = pointNeed(charStatus.p_STR+1)
         if(points >= point_next ){
             var tmp = charStatus.clone();
-            tmp.p_sub += 1
-            tmp.subV += 30
+            tmp.p_STR += 1
+            tmp.STR += 30
             tmp_rAtk = (tmp.realAtk() - based_atk) / point_next;
             if(tmp_rAtk > max_up){
                 max_up = tmp_rAtk;
@@ -288,7 +244,7 @@ function optim_Greddy(charStatus, points){
             }
         }
         set = new Set()
-        dfs(charStatus, max_point)
+        dfsDA(charStatus, max_point)
         max_v = 0
         point_used = max_point - max_left
         charStatus = max_status.clone()
@@ -302,32 +258,28 @@ function optim_Greddy(charStatus, points){
 
 
 /**
- *     constructor(mainV, subV, weapon, att, att_p, boss_a,   defense, base_ig, ctri_a,
-                p_main = 0, p_sub = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0) {
+ *     constructor(HP, STR, weapon, att, att_p, boss_a,   defense, base_ig, ctri_a,
+                p_HP = 0, p_STR = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0) {
  */
-// my = new CharStatus( 49760, 6475,  1.2,    3318, 0.97, 3.93+0.57, 3.8,    0.9658, 0.864 )
+// my = new CharStatusDA( 49760, 6475,  1.2,    3318, 0.97, 3.93+0.57, 3.8,    0.9658, 0.864 )
 
+// my = new CharStatusDA( 1044260, 874, 34000, 5.28, 200, 1.2,    3318, 0.97, 3.93+0.57, 3.8,    0.9658, 0.864 )
 
-// best_status = optim_Greddy(my, 1287)
+// best_status = optim_GreddyDA(my, 1287)
 // console.log(my.realAtk())
 
 
 
+// console.log()
 
 
-function allBlack(){
-    for(var i = 1; i < 20; ++i)
-        try {
-            document.querySelector('label[for="input' + i + '"]').style.color = 'black';
-        } catch (error) {
-            
-        }
-        
-}
-
-function example(){
-    document.getElementById("mainV").value = 49760;
-    document.getElementById("subV").value = 6475;
+function exampleDA(){
+    max_v = 0;
+    document.getElementById("HP").value = 1044260;
+    document.getElementById("HP_p").value = 528;
+    document.getElementById("HP_wo").value = 34000;
+    document.getElementById("STR").value = 874;
+    document.getElementById("level").value = 275;
     document.getElementById("att").value = 3318;
 
     document.getElementById("tot_a").value = 57;
@@ -340,12 +292,16 @@ function example(){
 }
   //  document.getElementById("att_p").value = 97;
 
-let best_status = 0;
-function calculate(){
+function calculateDA(){
     allBlack();
+    best_status = 0;
     document.getElementById("output").innerHTML = "計算中><";
-    mainV = parseFloat( document.getElementById("mainV").value );
-    subV = parseFloat( document.getElementById("subV").value );
+    HP = parseFloat( document.getElementById("HP").value );
+    HP_p = parseFloat( document.getElementById("HP_p").value ) / 100;
+    HP_wo = parseFloat( document.getElementById("HP_wo").value );
+    STR = parseFloat( document.getElementById("STR").value );
+    level = parseFloat( document.getElementById("level").value );
+
     att = parseFloat( document.getElementById("att").value );
     att_p = 1 // parseFloat( document.getElementById("att_p").value ) / 100;
     tot_a = parseFloat( document.getElementById("tot_a").value ) / 100;
@@ -355,13 +311,19 @@ function calculate(){
     defense = parseFloat( document.getElementById("defense").value ) / 100;
     points = parseInt( document.getElementById("points").value );
 
-   // console.log(mainV, subV, att, att_p, tot_a, boss_a, base_ig, ctri_a, defense)
-    if(isNaN(mainV+ subV+ att+ att_p+ tot_a+ boss_a+ base_ig+ ctri_a + defense) || mainV == 0 || subV == 0 || att == 0 ){
+    console.log(HP, STR, att, att_p, tot_a, boss_a, base_ig, ctri_a, defense)
+    if(isNaN(HP+ STR+ att+ att_p+ tot_a+ boss_a+ base_ig+ ctri_a + defense) || HP == 0 || STR == 0 || att == 0){
         document.getElementById("output").innerHTML = "輸入有錯！請檢查一下紅色的項目><";
-        if(isNaN(mainV) || mainV == 0) 
+        if(isNaN(HP) || HP == 0) 
             document.querySelector('label[for="input1"]').style.color = 'red';
-        if(isNaN(subV) || subV == 0) 
-            document.querySelector('label[for="input2"]').style.color = 'red';
+        if(isNaN(HP_p)) 
+            document.querySelector('label[for="input14"]').style.color = 'red';
+        if(isNaN(HP_wo)) 
+            document.querySelector('label[for="input11"]').style.color = 'red';
+        if(isNaN(STR) || STR == 0) 
+            document.querySelector('label[for="input13"]').style.color = 'red';
+        if(isNaN(level)) 
+            document.querySelector('label[for="input12"]').style.color = 'red';
         if(isNaN(att) || att == 0) 
             document.querySelector('label[for="input3"]').style.color = 'red';
         if(isNaN(att_p)) 
@@ -381,9 +343,9 @@ function calculate(){
     }else{
         allBlack();
         document.getElementById("output").innerHTML = "計算中>< <br> 目前推演至：" ;
-        my = new CharStatus( mainV, subV, 1, att, att_p, boss_a+tot_a, defense, base_ig, ctri_a )
-        best_status = optim_Greddy(my, points)
-        document.getElementById("output").innerHTML  = '主屬性　應升至：' + best_status[0].p_main + " 等<br><br>副屬性　應升至：" + best_status[0].p_sub
+        my = new CharStatusDA( HP, STR, HP_wo, HP_p, level, 1, att, att_p, boss_a+tot_a, defense, base_ig, ctri_a )
+        best_status = optim_GreddyDA(my, points)
+        document.getElementById("output").innerHTML  = '血量　　應升至：' + best_status[0].p_HP + " 等<br><br>力量　　應升至：" + best_status[0].p_STR
         document.getElementById("output").innerHTML += ' 等<br><br>爆擊傷害應升至：' + best_status[0].p_ctri + " 等<br><br>無視防禦應升至：" + best_status[0].p_ignore
         document.getElementById("output").innerHTML += ' 等<br><br>Boss傷害應升至：' + best_status[0].p_Bharm + " 等<br><br>總傷害　應升至：" + best_status[0].p_harm
         document.getElementById("output").innerHTML += ' 等<br><br>攻擊力　應升至：' + best_status[0].p_atk + " 等<br><br><br>剩餘點數：" + best_status[1]
--- js/Xenon.js
+++ js/Xenon.js
@@ -1,47 +1,10 @@
-function pointNeed(now){
-    switch (now){
-        case 1:
-            return 1
-        case 2:
-            return 2
-        case 3:
-            return 4
-        case 4:
-            return 8
-        case 5:
-            return 10
-        case 6:
-            return 15
-        case 7:
-            return 20
-        case 8:
-            return 25
-        case 9:
-            return 30
-        case 10:
-            return 35
-        case 11:
-            return 50
-        case 12:
-            return 65
-        case 13:
-            return 80
-        case 14:
-            return 95
-        case 15:
-            return 110
-        default:
-            return 1000000
-    }
-}
-
-
-class CharStatus {
-    constructor(mainV, subV, weapon, att, att_p, boss_a, defense, base_ig,  ctri_a,
-                p_main = 0, p_sub = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0, ignore = -1) {
+class CharStatusXenon {
+    constructor(mainV, subV, subV2, weapon, att, att_p, boss_a, defense, base_ig,  ctri_a,
+                p_main = 0, p_sub = 0, p_sub2 = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0, ignore = -1) {
 
         this.mainV = mainV;
         this.subV = subV;
+        this.subV2 = subV2;
         this.weapon = weapon;
         this.att = att;
         this.att_p = att_p;
@@ -56,6 +19,7 @@ class CharStatus {
 
         this.p_main = p_main
         this.p_sub = p_sub
+        this.p_sub2 = p_sub2
         this.p_ctri = p_ctri
         this.p_ignore = p_ignore
         this.p_harm = p_harm
@@ -65,7 +29,7 @@ class CharStatus {
     }
 
     base() {
-        return (4 * this.mainV + this.subV) * this.weapon * (this.att * (1 + this.att_p) ) * 0.01
+        return 4 * 1.5 * (this.mainV + this.subV + this.subV2) * this.weapon * (this.att * (1 + this.att_p) ) * 0.01
     }
     defense_c() {
         var ret = 1 - this.defense * ( 1 - this.ignore) // (1-0.86) * (1-ignore)
@@ -81,33 +45,19 @@ class CharStatus {
     }
 
     clone() {
-        return new CharStatus(this.mainV, this.subV, this.weapon, this.att, this.att_p, this.boss_a, this.defense, this.base_ig, this.ctri_a,
-            this.p_main, this.p_sub, this.p_ctri, this.p_ignore, this.p_harm, this.p_Bharm, this.p_atk, this.ignore)
+        return new CharStatusXenon(this.mainV, this.subV, this.subV2, this.weapon, this.att, this.att_p, this.boss_a, this.defense, this.base_ig, this.ctri_a,
+            this.p_main, this.p_sub, this.p_sub2, this.p_ctri, this.p_ignore, this.p_harm, this.p_Bharm, this.p_atk, this.ignore)
     }
     get_points(){
-        return "" + this.p_main + this.p_sub + this.p_ctri + this.p_ignore + this.p_harm + this.p_Bharm + this.p_atk
+        return "" + this.p_main + this.p_sub + this.p_sub2 + this.p_ctri + this.p_ignore + this.p_harm + this.p_Bharm + this.p_atk
     }
 }
 
 
-// let dict = {} // 現在配點 : 攻擊
-// function dfs(charStatus, points){
-//     point_name = charStatus.get_points()
-//     if(  dict.hasOwnProperty( point_name)) 
-//         return;
 
-//     var new_atk = charStatus.realAtk();
-//     dict[point_name] = new_atk
-
-let max_v = 0
-let max_left = 0
-let max_status = null
-let set = new Set()
-let t = 1
-function dfs(charStatus, points){
+function dfsXenon(charStatus, points){
     if(  set.has(charStatus.get_points() )) 
         return;
-    
     var new_atk = charStatus.realAtk();
     if( new_atk > max_v ){
         max_v = new_atk;
@@ -115,10 +65,10 @@ function dfs(charStatus, points){
         max_status = charStatus.clone();
        // console.log(">>", charStatus.p_main, charStatus.p_sub, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
     }
-   // console.log(charStatus.p_main, charStatus.p_sub, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
+ //  console.log(charStatus.p_main, charStatus.p_sub, charStatus.p_sub2, charStatus.p_ctri, charStatus.p_ignore, charStatus.p_harm, charStatus.p_Bharm, charStatus.p_atk)
 
     document.getElementById("output").innerHTML = "計算中>< <br> 目前推演至：" ;
-    document.getElementById("output").innerHTML += "" + charStatus.p_main + " " + charStatus.p_sub + " "  +charStatus.p_ctri + " "  + charStatus.p_ignore  + " "  + charStatus.p_harm + " "  + charStatus.p_Bharm + " "  + charStatus.p_atk
+    document.getElementById("output").innerHTML += "" + charStatus.p_main + " " + charStatus.p_sub + " " +charStatus.p_sub2 + " " +charStatus.p_ctri + " "  + charStatus.p_ignore  + " "  + charStatus.p_harm + " "  + charStatus.p_Bharm + " "  + charStatus.p_atk
 
     set.add( charStatus.get_points() )
 
@@ -128,23 +78,32 @@ function dfs(charStatus, points){
         var tmp = charStatus.clone();
         tmp.p_main += 1
         tmp.mainV += 30
-        dfs(tmp, points -  point_next )
+        dfsXenon(tmp, points -  point_next )
     }
-    // 副屬
+    // 副屬1
     point_next = pointNeed(charStatus.p_sub+1)
     if(points >= point_next ){
         var tmp = charStatus.clone();
         tmp.p_sub += 1
         tmp.subV += 30
-        dfs(tmp, points -  point_next )
+        dfsXenon(tmp, points -  point_next )
     }
+    // 副屬2
+    point_next = pointNeed(charStatus.p_sub2+1)
+    if(points >= point_next ){
+        var tmp = charStatus.clone();
+        tmp.p_sub2 += 1
+        tmp.subV2 += 30
+        dfsXenon(tmp, points -  point_next )
+    }
+
     // 爆傷
     point_next = pointNeed(charStatus.p_ctri+1)
     if(points >= point_next ){
         var tmp = charStatus.clone();
         tmp.p_ctri += 1
         tmp.ctri_a += 0.01
-        dfs(tmp, points -  point_next )
+        dfsXenon(tmp, points -  point_next )
     }
     // 無視
     point_next = pointNeed(charStatus.p_ignore+1)
@@ -153,7 +112,7 @@ function dfs(charStatus, points){
         tmp.ignore = tmp.base_ig
         tmp.p_ignore += 1
         tmp.add_ignore( tmp.p_ignore * 0.03 )
-        dfs(tmp, points -  point_next )
+        dfsXenon(tmp, points -  point_next )
 
     }
     // 傷害
@@ -162,7 +121,7 @@ function dfs(charStatus, points){
         var tmp = charStatus.clone();
         tmp.p_harm += 1
         tmp.boss_a += 0.03
-        dfs(tmp, points -  point_next )
+        dfsXenon(tmp, points -  point_next )
     }
     // B傷
     point_next = pointNeed(charStatus.p_Bharm+1)
@@ -173,7 +132,7 @@ function dfs(charStatus, points){
             tmp.boss_a += 0.03
         else
             tmp.boss_a += 0.04
-        dfs(tmp, points - point_next )
+        dfsXenon(tmp, points - point_next )
 
     }
     // 攻擊
@@ -182,11 +141,11 @@ function dfs(charStatus, points){
         var tmp = charStatus.clone();
         tmp.p_atk += 1
         tmp.att += 3
-        dfs(tmp, points - point_next )
+        dfsXenon(tmp, points - point_next )
     }
     
 }
-function optim_Greddy(charStatus, points){
+function optim_GreddyXenon(charStatus, points){
     last_point = -1
 
     while(last_point != points && points > 0){
@@ -209,12 +168,24 @@ function optim_Greddy(charStatus, points){
                 max_point = point_next;
             }
         }
-        // 副屬
+        // 副屬1
         point_next = pointNeed(charStatus.p_sub+1)
         if(points >= point_next ){
             var tmp = charStatus.clone();
             tmp.p_sub += 1
             tmp.subV += 30
+            tmp_rAtk = (tmp.realAtk() - based_atk) / point_next;
+            if(tmp_rAtk > max_up){
+                max_up = tmp_rAtk;
+                max_point = point_next;
+            }
+        }
+        // 副屬2
+        point_next = pointNeed(charStatus.p_sub2+1)
+        if(points >= point_next ){
+            var tmp = charStatus.clone();
+            tmp.p_sub2 += 1
+            tmp.subV2 += 30
             tmp_rAtk = (tmp.realAtk() - based_atk) / point_next;
             if(tmp_rAtk > max_up){
                 max_up = tmp_rAtk;
@@ -288,7 +259,7 @@ function optim_Greddy(charStatus, points){
             }
         }
         set = new Set()
-        dfs(charStatus, max_point)
+        dfsXenon(charStatus, max_point)
         max_v = 0
         point_used = max_point - max_left
         charStatus = max_status.clone()
@@ -305,29 +276,21 @@ function optim_Greddy(charStatus, points){
  *     constructor(mainV, subV, weapon, att, att_p, boss_a,   defense, base_ig, ctri_a,
                 p_main = 0, p_sub = 0, p_ctri = 0, p_ignore = 0, p_harm = 0, p_Bharm = 0, p_atk = 0) {
  */
-// my = new CharStatus( 49760, 6475,  1.2,    3318, 0.97, 3.93+0.57, 3.8,    0.9658, 0.864 )
+// my = new CharStatusXenon( 49760, 6475,  1.2,    3318, 0.97, 3.93+0.57, 3.8,    0.9658, 0.864 )
 
 
-// best_status = optim_Greddy(my, 1287)
+// best_status = optim_GreddyXenon(my, 1287)
 // console.log(my.realAtk())
 
 
 
 
 
-function allBlack(){
-    for(var i = 1; i < 20; ++i)
-        try {
-            document.querySelector('label[for="input' + i + '"]').style.color = 'black';
-        } catch (error) {
-            
-        }
-        
-}
 
-function example(){
-    document.getElementById("mainV").value = 49760;
-    document.getElementById("subV").value = 6475;
+function exampleXenon(){
+    document.getElementById("mainV").value = 20000;
+    document.getElementById("subV").value = 17600;
+    document.getElementById("subV2").value = 20000;
     document.getElementById("att").value = 3318;
 
     document.getElementById("tot_a").value = 57;
@@ -340,12 +303,12 @@ function example(){
 }
   //  document.getElementById("att_p").value = 97;
 
-let best_status = 0;
-function calculate(){
+function calculateXenon(){
     allBlack();
     document.getElementById("output").innerHTML = "計算中><";
     mainV = parseFloat( document.getElementById("mainV").value );
     subV = parseFloat( document.getElementById("subV").value );
+    subV2 = parseFloat( document.getElementById("subV2").value );
     att = parseFloat( document.getElementById("att").value );
     att_p = 1 // parseFloat( document.getElementById("att_p").value ) / 100;
     tot_a = parseFloat( document.getElementById("tot_a").value ) / 100;
@@ -355,13 +318,15 @@ function calculate(){
     defense = parseFloat( document.getElementById("defense").value ) / 100;
     points = parseInt( document.getElementById("points").value );
 
-   // console.log(mainV, subV, att, att_p, tot_a, boss_a, base_ig, ctri_a, defense)
-    if(isNaN(mainV+ subV+ att+ att_p+ tot_a+ boss_a+ base_ig+ ctri_a + defense) || mainV == 0 || subV == 0 || att == 0 ){
+    console.log(mainV, subV, att, att_p, tot_a, boss_a, base_ig, ctri_a, defense)
+    if(isNaN(mainV+ subV + subV2 + att+ att_p+ tot_a+ boss_a+ base_ig+ ctri_a + defense) || att <= 0 || mainV <= 0 || subV<= 0 || subV2 <= 0 ){
         document.getElementById("output").innerHTML = "輸入有錯！請檢查一下紅色的項目><";
         if(isNaN(mainV) || mainV == 0) 
             document.querySelector('label[for="input1"]').style.color = 'red';
         if(isNaN(subV) || subV == 0) 
             document.querySelector('label[for="input2"]').style.color = 'red';
+        if(isNaN(subV2) || subV2 == 0) 
+            document.querySelector('label[for="input15"]').style.color = 'red';
         if(isNaN(att) || att == 0) 
             document.querySelector('label[for="input3"]').style.color = 'red';
         if(isNaN(att_p)) 
@@ -381,9 +346,10 @@ function calculate(){
     }else{
         allBlack();
         document.getElementById("output").innerHTML = "計算中>< <br> 目前推演至：" ;
-        my = new CharStatus( mainV, subV, 1, att, att_p, boss_a+tot_a, defense, base_ig, ctri_a )
-        best_status = optim_Greddy(my, points)
-        document.getElementById("output").innerHTML  = '主屬性　應升至：' + best_status[0].p_main + " 等<br><br>副屬性　應升至：" + best_status[0].p_sub
+        my = new CharStatusXenon( mainV, subV, subV2, 1, att, att_p, boss_a+tot_a, defense, base_ig, ctri_a )
+        best_status = optim_GreddyXenon(my, points)
+        document.getElementById("output").innerHTML  = '力量　　應升至：' + best_status[0].p_main + " 等<br><br>敏捷　　應升至：" + best_status[0].p_sub
+        document.getElementById("output").innerHTML += ' 等<br><br>幸運　　應升至：' + best_status[0].p_sub2;
         document.getElementById("output").innerHTML += ' 等<br><br>爆擊傷害應升至：' + best_status[0].p_ctri + " 等<br><br>無視防禦應升至：" + best_status[0].p_ignore
         document.getElementById("output").innerHTML += ' 等<br><br>Boss傷害應升至：' + best_status[0].p_Bharm + " 等<br><br>總傷害　應升至：" + best_status[0].p_harm
         document.getElementById("output").innerHTML += ' 等<br><br>攻擊力　應升至：' + best_status[0].p_atk + " 等<br><br><br>剩餘點數：" + best_status[1]
